69a026fd261af4ab2616ce0143679ee7b00816dd
Chris Hilsdon <chrish@xigen.co.uk>
2016-01-21 16:38:42 +0000

    Added Clients namespace trait

--- src/Shortcuts/Client.php
+++ src/Shortcuts/Client.php
@@ -3,13 +3,55 @@ namespace Xigen\Shortcuts;
 
 trait Client
 {
+    // Client.php
+
+    // Users.php
+    /**
+     * TODO: Add POST options
+     */
+    public function clientUsersAdd()
+    {
+        return $this->get('client/users/add/');
+    }
+
+    public function clientUsersDelete($Hash)
+    {
+        return $this->get('client/users/delete/' . $Hash);
+    }
+
+    /**
+     * TODO: Add POST options
+     */
+    public function clientUsersSave($Hash)
+    {
+        return $this->get('client/users/save/');
+    }
+
+    /**
+     * TODO: Add POST options
+     */
+    public function clientUsersImport($Hash)
+    {
+        return $this->get('client/users/import/');
+    }
+
+    public function clientUsersExport($Hash)
+    {
+        return $this->get('client/users/export/' . $Hash);
+    }
+
+    public function clientUsersGet($Hash)
+    {
+        return $this->get('client/users/getall/' . $Hash);
+    }
+
     public function clientUsersGetAll($start = 0, $limit = 25)
     {
         return $this->get('client/users/getall/null/' . $start . '/ ' . $limit);
     }
 
     /**
-     * Keeping backwords compatibility of this function
+     * Keeping backwords compatibility of clientUsersGetAll() function
      */
     public function clientGetAll($start = 0, $limit = 25)
     {
